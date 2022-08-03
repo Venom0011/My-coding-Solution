9dfe3007d9c67476dbd78984057c9b6911c5458b
Venom0011 <abhishekmahajan255@gmail.com>
2022-08-03 08:01:45 +0530

    push

---
+++ PetyaandStringsCodedforces.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1;
+    string s2;
+    
+    cin>>s1>>s2;
+    
+    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+    transform(s2.begin(),s2.end(),s2.begin(),::tolower);
+    
+    if(s1<s2){
+        cout<<-1;
+    }
+    if(s2<s1){
+        cout<<1;
+    }
+    if(s2==s1){
+        cout<<0;
+    }
+    
+    return 0;
+}
