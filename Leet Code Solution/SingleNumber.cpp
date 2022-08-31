4597c94300041516e836fd9058150232930d6430
Venom0011 <abhishekmahajan255@gmail.com>
2022-08-31 17:22:51 +0530

    tricky prob

---
+++ Leet Code Solution/SingleNumber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+#include<bits/stdc++.h>
+using namespace std;
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        map<int,int>a;          // a map is used to maintain the count of the numbers
+        int n=nums.size();      // and the numbers that are repeated 
+        for(int i=0;i<n;i++){
+            a[nums[i]]++;
+        }
+        for(auto pr:a){
+            if(pr.second==1){   // so formapwhose first value is 1 is returned else 0
+                return pr.first;    //is returned 
+            }
+        }
+        return 0;
+    }
+};
